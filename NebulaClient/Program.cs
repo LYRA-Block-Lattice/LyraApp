ff5304a8f8e0da538856ae787220ff1b6911bf3d
Wizard Young <rcvbuf@gmail.com>
2021-12-11 17:09:13 +0800

    client the new start

--- NebulaClient/Program.cs
+++ NebulaClient/Program.cs
@@ -1,13 +1,11 @@
 using Blazored.LocalStorage;
-using Blazorise;
-using Blazorise.Bootstrap5;
-using Blazorise.Icons.FontAwesome;
 using Fluxor;
 using Lyra.Core.API;
 using Lyra.Data.API;
 using Lyra.Data.Crypto;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using NebulaClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -22,15 +20,10 @@ builder.Services.AddBlazoredLocalStorage();
 var networkid = builder.Configuration["network"];
 builder.Services.AddTransient<ILyraAPI>(a => LyraRestClient.Create(networkid, Environment.OSVersion.ToString(), "Nebula", "1.0"/*, $"http://nebula.{networkid}.lyra.live:{Neo.Settings.Default.P2P.WebAPI}/api/Node/"*/));
 
-builder.Services.AddBlazorise(options =>
-{
-	options.ChangeTextOnKeyPress = true; // optional
-})
-  .AddBootstrap5Providers()
-  .AddFontAwesomeIcons();
-
 var currentAssembly = typeof(Program).Assembly;
 var libAssembly = typeof(UserLibrary.Data.WalletView).Assembly;
 builder.Services.AddFluxor(options => options.ScanAssemblies(libAssembly, currentAssembly));
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
