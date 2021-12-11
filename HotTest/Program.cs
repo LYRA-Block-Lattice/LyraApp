5ed39712804dbd9bd9d60fb86a503579aa8b57ff
Wizard Young <rcvbuf@gmail.com>
2021-12-11 22:32:15 +0800

    add a test app to enable hotreload

---
+++ HotTest/Program.cs
@@ -0,0 +1,47 @@
+using Blazored.LocalStorage;
+using Fluxor;
+using Lyra.Core.API;
+using Lyra.Data.API;
+using Lyra.Data.Crypto;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+Signatures.Switch(true);
+
+builder.Services.AddBlazoredLocalStorage();
+var networkid = builder.Configuration["network"];
+builder.Services.AddTransient<ILyraAPI>(a => LyraRestClient.Create(networkid, Environment.OSVersion.ToString(), "Nebula", "1.0"/*, $"http://nebula.{networkid}.lyra.live:{Neo.Settings.Default.P2P.WebAPI}/api/Node/"*/));
+
+var currentAssembly = typeof(Program).Assembly;
+var libAssembly = typeof(UserLibrary.Data.WalletView).Assembly;
+builder.Services.AddFluxor(options => options.ScanAssemblies(libAssembly, currentAssembly));
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
