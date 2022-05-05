1d2de9f094cffac49eeaa295d6764dc2ed52624b
wizd <rcvbuf@gmail.com>
2022-05-05 18:31:54 +0800

    added validator to new order

--- HotTest/wwwroot/js/util.js
+++ HotTest/wwwroot/js/util.js
@@ -2,3 +2,14 @@
     //var element = document.getElementById(id);
     element.scrollTop = element.scrollHeight;
 }
+
+window.clipboardCopy = {
+    copyText: function (text) {
+        navigator.clipboard.writeText(text).then(function () {
+            alert("Copied to clipboard!");
+        })
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+};
