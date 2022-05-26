b07b9edb1e38c544c9cbd27315f31ce5c97f1cbb
wizd <rcvbuf@gmail.com>
2022-05-26 22:53:17 +0800

    added UI for trusted dealers

--- UserLibrary/Data/NebulaConsts.cs
+++ UserLibrary/Data/NebulaConsts.cs
@@ -21,8 +21,9 @@ namespace UserLibrary.Data
         {
             "devnet" => new[]
                 {
-                "L9vh5kuijpaDiqYAaHoV6EejAL3qUXF15JrSR1LvHien3h4fHR3B9p65ubF9AgQnnMzUxdLbDTPtjwpbxB5SPPtSaF4wMr"
-                //"LDLRr2a1TUgV6ccaoP3tKh9LuarGq3Ksxi7RKWnXv6zmPoNqjXDK2MxL81xP99aVDtMDCvUVtAw7BLUXUnUKuqGk9aSYHP"
+                "L9vh5kuijpaDiqYAaHoV6EejAL3qUXF15JrSR1LvHien3h4fHR3B9p65ubF9AgQnnMzUxdLbDTPtjwpbxB5SPPtSaF4wMr",
+                "L9ZapWEuzZH9nqD6qYEyTYsGKRa3Rif9foEMLC7VNaWcqRQQX41HLdXev6V5TR4ZMWoSWCSTz9pEHh4U8gG1H6HruyC5sC"
+                //"LEn8GndA1k4QJQ7jNp6Nm71DuQCnZLcCuTZcqSBaA8KS1Jm8RPixwrK79814gcGRXDW1Cf7Vh9fcyqah39ZDBPhtrgq2ew"
                 },
             _ => new string[] { },
         };            
