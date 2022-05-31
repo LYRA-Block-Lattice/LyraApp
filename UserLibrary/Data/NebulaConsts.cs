62ab62c35cf97cddc8696d704d27ccaec91bdde8
wizd <rcvbuf@gmail.com>
2022-05-31 20:33:13 +0800

    mainnet fix

--- UserLibrary/Data/NebulaConsts.cs
+++ UserLibrary/Data/NebulaConsts.cs
@@ -20,6 +20,10 @@ namespace UserLibrary.Data
 
         public string[] TrustedDealerIds => _config["network"] switch
         {
+            "mainnet" => new [] 
+            {
+                "L7hfFpQTndpjUDLwUodRNLbo27BDEu6xPyQF6kcVxHw4u7BamLKXXHB8fnx9mai1mAR725bQd3TeTvkiDDz8KbWsfsYA79"
+            },
             "testnet" => new[]
             {
                 "L9YsTcVwnff8JgqynoVGnMP2ePfiKPrqiyDEAiVY842pQaC3uxwzK7jyck3JiHw7tBoMS3G4o3EHDESvUb2yodCHNYf9rT"
@@ -30,7 +34,7 @@ namespace UserLibrary.Data
                 "L9ZapWEuzZH9nqD6qYEyTYsGKRa3Rif9foEMLC7VNaWcqRQQX41HLdXev6V5TR4ZMWoSWCSTz9pEHh4U8gG1H6HruyC5sC"
                 //"LEn8GndA1k4QJQ7jNp6Nm71DuQCnZLcCuTZcqSBaA8KS1Jm8RPixwrK79814gcGRXDW1Cf7Vh9fcyqah39ZDBPhtrgq2ew"
                 },
-            _ => new string[] { },
+            _ => throw new Exception($"unknown network id: {_config["network"]}")
         };            
     }
 }
