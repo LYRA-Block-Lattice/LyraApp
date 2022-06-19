1607ce5be877e82e46f196082a9b6e00ba95c0a2
wizd <rcvbuf@gmail.com>
2022-06-20 00:30:31 +0800

    fix create new order

---
+++ UserLibrary/Data/PageWithContextMenu.cs
@@ -0,0 +1,40 @@
+﻿using Fluxor;
+using Fluxor.Blazor.Web.Components;
+using Microsoft.AspNetCore.Components;
+using Nebula.Store.WebWalletUseCase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLibrary.Data
+{
+    public class PageWithContextMenu : FluxorComponent
+    {
+        protected string _title = "Lyra App";
+
+        [Inject]
+        private IDispatcher Dispatcher { get; set; }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if(firstRender)
+            {
+                Dispatcher.Dispatch(new WebWalletChangeTitleAction
+                {
+                    title = _title,
+                    menus = CreateContextMenu()
+                });
+            }
+
+            base.OnAfterRender(firstRender);
+        }
+
+        protected virtual Dictionary<string, string> CreateContextMenu()
+        {
+            return null;
+        }
+    }
+
+}
