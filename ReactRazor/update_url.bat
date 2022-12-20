d7d569466026b87d0c00612ec34348a3ba778afb
wizd <rcvbuf@gmail.com>
2022-12-21 00:00:41 +0800

    template

--- ReactRazor/update_url.bat
+++ ReactRazor/update_url.bat
@@ -1,5 +1,5 @@
 call .\node_modules\.bin\replace-in-file /=\"\.\.\/asserts\//g =\"_content/ReactRazor/asserts/ .\src\components\**.** --isRegex --verbose
 call .\node_modules\.bin\replace-in-file /=\"\.\.\/asserts\//g =\"_content/ReactRazor/asserts/ .\src\pages\**.** --isRegex --verbose
 
-call .\node_modules\.bin\replace-in-file /\.\.\/\.\.\/public\/asserts\//g _content/ReactRazor/asserts/ .\src\components\**.** --isRegex --verbose
-call .\node_modules\.bin\replace-in-file /\.\.\/\.\.\/public\/asserts\//g _content/ReactRazor/asserts/ .\src\pages\**.** --isRegex --verbose
+rem call .\node_modules\.bin\replace-in-file /\.\.\/\.\.\/public\/asserts\//g _content/ReactRazor/asserts/ .\src\components\**.** --isRegex --verbose
+rem call .\node_modules\.bin\replace-in-file /\.\.\/\.\.\/public\/asserts\//g _content/ReactRazor/asserts/ .\src\pages\**.** --isRegex --verbose
