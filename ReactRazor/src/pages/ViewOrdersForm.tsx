b46d4db2041ef81a009d62223f0ace56d89cc08b
wizd <rcvbuf@gmail.com>
2022-12-30 02:00:36 +0800

    build

--- ReactRazor/src/pages/ViewOrdersForm.tsx
+++ ReactRazor/src/pages/ViewOrdersForm.tsx
@@ -11,6 +11,7 @@ declare const window: customWindow;
 interface IOrder {
   time: string;
   hash: string;
+  hash2: string;
   status: string;
   offering: string;
   biding: string;
@@ -51,79 +52,81 @@ const ViewOrdersForm: FunctionComponent = () => {
           <div className="utility-button6">New</div>
         </button>
       </div>
-      <div className="ordercard3">
-        <div className="order-brief-section">
-          <button className="order-banner-wrapper">
-            <div className="order-banner">
-              <button className="order-image">
+      {orders.map((order) =>
+        <div className="ordercard3">
+          <div className="order-brief-section">
+            <button className="order-banner-wrapper">
+              <div className="order-banner">
+                <button className="order-image">
+                  <img
+                    className="icbaseline-generating-tokens-icon"
+                    alt=""
+                    src="_content/ReactRazor/asserts/icbaselinegeneratingtokens.svg"
+                  />
+                  <img
+                    className="order-image-child"
+                    alt=""
+                    src="_content/ReactRazor/asserts/arrow-1.svg"
+                  />
+                  <img
+                    className="icbaseline-generating-tokens-icon"
+                    alt=""
+                    src="_content/ReactRazor/asserts/carbonuserservicedesk.svg"
+                  />
+                </button>
+                <div className="order-status">
+                  <b className="open3">{order.status}</b>
+                </div>
+              </div>
+            </button>
+            <div className="frame-parent">
+              <div className="btc-parent">
+                <b className="btc">{order.offering}</b>
                 <img
-                  className="icbaseline-generating-tokens-icon"
+                  className="frame-child"
                   alt=""
-                  src="_content/ReactRazor/asserts/icbaselinegeneratingtokens.svg"
+                  src="_content/ReactRazor/asserts/arrow-2.svg"
                 />
-                <img
-                  className="order-image-child"
-                  alt=""
-                  src="_content/ReactRazor/asserts/arrow-1.svg"
-                />
-                <img
-                  className="icbaseline-generating-tokens-icon"
-                  alt=""
-                  src="_content/ReactRazor/asserts/carbonuserservicedesk.svg"
-                />
-              </button>
-              <div className="order-status">
-                <b className="open3">Open</b>
+                <b className="tetherusdt">{order.biding}</b>
+              </div>
+              <div className="group-parent">
+                <div className="frame-group">
+                  <div className="price-1323-wrapper">
+                    <div className="btc">Price: {order.price}</div>
+                  </div>
+                  <div className="limit-min-1323-wrapper">
+                    <div className="btc">Limit Min: {order.limitmin}</div>
+                  </div>
+                </div>
+                <div className="frame-container">
+                  <div className="price-1323-wrapper">
+                    <div className="btc">Amount: {order.amount}</div>
+                  </div>
+                  <div className="limit-min-1323-wrapper">
+                    <div className="btc">Limit Max: {order.limitmax}</div>
+                  </div>
+                </div>
+                <div className="group-div">
+                  <div className="price-1323-wrapper">
+                    <div className="btc">Sold: 3.2</div>
+                  </div>
+                  <div className="limit-min-1323-wrapper">
+                    <div className="btc">Left: 3.2</div>
+                  </div>
+                </div>
               </div>
             </div>
-          </button>
-          <div className="frame-parent">
-            <div className="btc-parent">
-              <b className="btc">BTC</b>
-              <img
-                className="frame-child"
-                alt=""
-                src="_content/ReactRazor/asserts/arrow-2.svg"
-              />
-              <b className="tetherusdt">tether/USDT</b>
+          </div>
+          <div className="frame-parent1">
+            <div className="time-12292022-102537-am-wrapper">
+              <div className="btc">Time: {order.time}</div>
             </div>
-            <div className="group-parent">
-              <div className="frame-group">
-                <div className="price-1323-wrapper">
-                  <div className="btc">Price: 1,323</div>
-                </div>
-                <div className="limit-min-1323-wrapper">
-                  <div className="btc">Limit Min: 1,323</div>
-                </div>
-              </div>
-              <div className="frame-container">
-                <div className="price-1323-wrapper">
-                  <div className="btc">Amount: 3.2</div>
-                </div>
-                <div className="limit-min-1323-wrapper">
-                  <div className="btc">Limit Max: 3.2</div>
-                </div>
-              </div>
-              <div className="group-div">
-                <div className="price-1323-wrapper">
-                  <div className="btc">Sold: 3.2</div>
-                </div>
-                <div className="limit-min-1323-wrapper">
-                  <div className="btc">Left: 3.2</div>
-                </div>
-              </div>
+            <div className="hash-38ae3ef3-wrapper">
+              <div className="btc">Hash: {order.hash2}</div>
             </div>
           </div>
         </div>
-        <div className="frame-parent1">
-          <div className="time-12292022-102537-am-wrapper">
-            <div className="btc">Time: 12/29/2022 10:25:37 AM</div>
-          </div>
-          <div className="hash-38ae3ef3-wrapper">
-            <div className="btc">Hash: 38AE...3EF3</div>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
